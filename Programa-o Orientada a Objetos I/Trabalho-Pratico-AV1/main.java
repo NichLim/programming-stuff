f6344f655ab3521d33ed2790605f8c40d164ae0e
GabrielJulio <43018141+GabrielJulio@users.noreply.github.com>
2018-09-10 19:39:04 -0300

    Create main.java

---
+++ Programa-o Orientada a Objetos I/Trabalho-Pratico-AV1/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+  
+  //this maybe the beggining...(By GoodJob)
+  //Do u want 2 use VoIp on that saturday (15) or sunday (16) or even another day?(By GoodJob)
+  //Do you want to one give status updates for the other when we change something through Whatsapp?(By GoodJob)
+  //I'm doing those comments to make it more... unique, like he said to us do.(By GoodJob)
+  //Is this NiCoLaS, isn't you? (By GoodJob)
+  
+  }
+}
